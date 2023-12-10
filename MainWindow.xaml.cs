e40858821b83f7fb1aafc261675978e29dc96f00
jimmy <jimmyeao@gmail.com>
2023-12-10 11:22:57 +0000

    First MVP

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,23 +1,27 @@
-﻿using System;
-using System.Diagnostics;
+﻿using Microsoft.Win32;
+using Newtonsoft.Json;
+using System;
+using System.ComponentModel;
+using System.IO;
 using System.Linq;
-using System.Security.Cryptography;
-using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
-using TEAMS2HA.API;
-using TEAMS2HA.Properties;
-using Hardcodet.Wpf.TaskbarNotification;
-using System.Windows.Controls;
-using System.Windows.Media.Imaging;
-using System.ComponentModel;
-using Microsoft.Win32;
 
 namespace TEAMS2HA
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
+    public class AppSettings
+    {
+        public bool RunAtWindowsBoot { get; set; }
+        public bool RunMinimized { get; set; }
+        public string Theme { get; set; }
+        public string HomeAssistantToken { get; set; }
+        public string HomeAssistantURL { get; set; }
+        public string TeamsToken { get; set; }
+    }
+
     public partial class MainWindow : Window
     {
         #region Private Fields
@@ -28,6 +32,8 @@ namespace TEAMS2HA
         private string _teamsApiKey;
         private API.WebSocketClient _teamsClient;
         private bool isDarkTheme = false;
+        private string _settingsFilePath;
+        private AppSettings _settings;
 
         #endregion Private Fields
 
@@ -35,21 +41,19 @@ namespace TEAMS2HA
 
         public MainWindow()
         {
-            if (Properties.Settings.Default.FirstTimeRunningThisVersion)
-            {
-                Properties.Settings.Default.Upgrade();
-                Properties.Settings.Default.FirstTimeRunningThisVersion = false;
-                Properties.Settings.Default.Save();
-            }
+            var localAppData = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData);
+            var appDataFolder = Path.Combine(localAppData, "YourAppName");
+            Directory.CreateDirectory(appDataFolder); // Ensure the directory exists
+            _settingsFilePath = Path.Combine(appDataFolder, "settings.json");
+            _settings = LoadSettings();
 
             this.InitializeComponent();
-            ApplyTheme(Properties.Settings.Default.Theme);
+            //ApplyTheme(Properties.Settings.Default.Theme);
             this.Loaded += MainPage_Loaded;
             string iconPath = System.IO.Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Square150x150Logo.scale-200.ico");
             MyNotifyIcon.Icon = new System.Drawing.Icon(iconPath);
             InitializeConnections();
         }
-
 
         #endregion Public Constructors
 
@@ -58,11 +62,18 @@ namespace TEAMS2HA
         public async Task InitializeConnections()
         {
             // Other initialization code...
+            await initializeteamsconnection();
+            await InitializeHomeAssistantConnection();
+            // Other initialization code...
+        }
+
+        private async Task initializeteamsconnection()
+        {
             if (_teamsClient != null && _teamsClient.IsConnected)
             {
                 return; // Already connected, no need to reinitialize
             }
-            string teamsToken = TokenStorage.GetTeamsToken();
+            string teamsToken = _settings.TeamsToken;
             if (string.IsNullOrEmpty(teamsToken))
             {
                 // If the Teams token is not set, then we can't connect to Teams
@@ -72,15 +83,41 @@ namespace TEAMS2HA
             //var uri = new Uri("ws://localhost:8124?protocol-version=2.0.0&manufacturer=JimmyWhite&device=PC&app=THFHA&app-version=2.0.26");
             var uri = new Uri($"ws://localhost:8124?token={teamsToken}&protocol-version=2.0.0&manufacturer=JimmyWhite&device=PC&app=THFHA&app-version=2.0.26");
             var state = new API.State();  // You would initialize this as necessary
-            _teamsClient = new API.WebSocketClient(uri, state);
-            _teamsClient.ConnectionStatusChanged += TeamsConnectionStatusChanged;
+            _teamsClient = new API.WebSocketClient(uri, state, _settingsFilePath);
 
-            // Other initialization code...
+            _teamsClient.ConnectionStatusChanged += TeamsConnectionStatusChanged;
         }
 
         #endregion Public Methods
 
         #region Private Methods
+
+        private AppSettings LoadSettings()
+        {
+            if (File.Exists(_settingsFilePath))
+            {
+                string json = File.ReadAllText(_settingsFilePath);
+                return JsonConvert.DeserializeObject<AppSettings>(json);
+            }
+            else
+            {
+                return new AppSettings(); // Defaults if file does not exist
+            }
+        }
+
+        private void SaveSettings()
+        {
+            _settings.RunAtWindowsBoot = RunAtWindowsBootCheckBox.IsChecked ?? false;
+            _settings.RunMinimized = RunMinimisedCheckBox.IsChecked ?? false;
+            _settings.HomeAssistantToken = HomeassistantTokenBox.Text;
+            _settings.HomeAssistantURL = HomeassistantURLBox.Text;
+            _settings.TeamsToken = TeamsApiKeyBox.Text;
+            _settings.Theme = isDarkTheme ? "Dark" : "Light";
+
+            string json = JsonConvert.SerializeObject(_settings, Formatting.Indented);
+            File.WriteAllText(_settingsFilePath, json);
+        }
+
         private void MyNotifyIcon_Click(object sender, EventArgs e)
         {
             if (this.WindowState == WindowState.Minimized)
@@ -95,7 +132,6 @@ namespace TEAMS2HA
                 this.WindowState = WindowState.Minimized;
             }
         }
-
 
         protected override void OnClosing(CancelEventArgs e)
         {
@@ -120,47 +156,6 @@ namespace TEAMS2HA
             base.OnStateChanged(e);
         }
 
-
-        private async Task LoadSettingsAsync()
-        {
-            // Assuming you have settings like HomeassistantURL, RunAtWindowsBoot, and RunMinimised defined
-
-            // Load Homeassistant URL
-            _homeassistantURL = Properties.Settings.Default.HomeassistantURL;
-            HomeassistantURLBox.Text = _homeassistantURL;
-
-            // Load CheckBox values
-            RunAtWindowsBootCheckBox.IsChecked = Properties.Settings.Default.RunAtWindowsBoot;
-            
-            RunMinimisedCheckBox.IsChecked = Properties.Settings.Default.RunMinimised;
-
-            if (RunMinimisedCheckBox.IsChecked == true)
-            {
-                // Start the window minimized and hide it
-                this.WindowState = WindowState.Minimized;
-                this.Hide();
-                MyNotifyIcon.Visibility = Visibility.Visible; // Show the NotifyIcon in the system tray
-            }
-            // Load Homeassistant Token (assuming you're using TokenStorage for this)
-            _homeassistantToken = TokenStorage.GetHomeassistantToken();
-            HomeassistantTokenBox.Text = _homeassistantToken;
-            // Load Teams Token
-            _teamsApiKey = TokenStorage.GetTeamsToken();
-            TeamsApiKeyBox.Text = _teamsApiKey;
-            // Rest of your logic...
-            if (!string.IsNullOrEmpty(_homeassistantURL) && !string.IsNullOrEmpty(_homeassistantToken))
-            {
-                var homeAssistant = new TEAMS2HA.API.HomeAssistant(_homeassistantURL, _homeassistantToken);
-                _homeAssistant = new TEAMS2HA.API.HomeAssistant(_homeassistantURL, _homeassistantToken);
-                var connectionSuccessful = await homeAssistant.CheckConnectionAsync();
-                HomeassistantConnectionStatus.Text = connectionSuccessful ? "Homeassistant: Connected" : "Homeassistant: Disconnected";
-                if (connectionSuccessful)
-                {
-                    await _homeAssistant.Start();
-                }
-            }
-            
-        }
         private async Task SetStartupAsync(bool startWithWindows)
         {
             await Task.Run(() =>
@@ -185,13 +180,22 @@ namespace TEAMS2HA
                 }
             });
         }
+
         private async void MainPage_Loaded(object sender, RoutedEventArgs e)
         {
-            await LoadSettingsAsync();
-            if (RunMinimisedCheckBox.IsChecked == true) // Check your run minimized setting
-            {
-                // Instead of minimizing, keep the app running in the background Show a toast
-                // notification to inform the user
+            //LoadSettings();
+
+            RunAtWindowsBootCheckBox.IsChecked = _settings.RunAtWindowsBoot;
+            RunMinimisedCheckBox.IsChecked = _settings.RunMinimized;
+            HomeassistantTokenBox.Text = _settings.HomeAssistantToken;
+            HomeassistantURLBox.Text = _settings.HomeAssistantURL;
+            TeamsApiKeyBox.Text = _settings.TeamsToken;
+            ApplyTheme(_settings.Theme);
+            if (RunMinimisedCheckBox.IsChecked == true)
+            {// Start the window minimized and hide it
+                this.WindowState = WindowState.Minimized;
+                this.Hide();
+                MyNotifyIcon.Visibility = Visibility.Visible; // Show the NotifyIcon in the system tray
             }
         }
 
@@ -205,36 +209,7 @@ namespace TEAMS2HA
         {
             SaveSettings();
         }
-        private void SaveSettings()
-        {
-            // Check if the Teams API key has changed
-            string currentTeamsToken = TokenStorage.GetTeamsToken();
-            bool teamsTokenChanged = currentTeamsToken != TeamsApiKeyBox.Text;
-            if (teamsTokenChanged)
-            {
-                TokenStorage.SaveTeamsToken(TeamsApiKeyBox.Text);
-                InitializeConnections();
-            }
 
-            // Store the Homeassistant token securely
-            if (!string.IsNullOrEmpty(HomeassistantTokenBox.Text))
-            {
-                TokenStorage.SaveHomeassistantToken(HomeassistantTokenBox.Text);
-            }
-
-            _homeassistantToken = HomeassistantTokenBox.Text;
-
-            // Store the Homeassistant URL
-            Properties.Settings.Default.HomeassistantURL = HomeassistantURLBox.Text;
-            _homeassistantURL = HomeassistantURLBox.Text;
-           
-            // Save CheckBox values
-            Properties.Settings.Default.RunAtWindowsBoot = RunAtWindowsBootCheckBox.IsChecked.Value;
-            Properties.Settings.Default.RunMinimised = RunMinimisedCheckBox.IsChecked.Value;
-            _ = SetStartupAsync(Properties.Settings.Default.RunAtWindowsBoot);
-            Properties.Settings.Default.Theme = isDarkTheme ? "Dark" : "Light";
-            Properties.Settings.Default.Save();
-        }
         private void TeamsConnectionStatusChanged(bool isConnected)
         {
             // The UI needs to be updated on the main thread.
@@ -244,47 +219,57 @@ namespace TEAMS2HA
             });
         }
 
+        // This method is called when the TestHomeassistantConnection button is clicked
         private async void TestHomeassistantConnection_Click(object sender, RoutedEventArgs e)
         {
-            // Implement the code to test the connection to Home Assistant If the connection is
-            // successful, update the HomeassistantConnectionStatus
-            // Example: HomeassistantConnectionStatus.Text = "Homeassistant: Connected";
+            // Get the Homeassistant token from the HomeassistantTokenBox
             _homeassistantToken = HomeassistantTokenBox.Text;
+
+            // If the token is empty or null, return and do nothing
             if (string.IsNullOrEmpty(_homeassistantToken))
             {
                 return;
             }
-            var homeAssistant = new TEAMS2HA.API.HomeAssistant(_homeassistantURL, _homeassistantToken);
-            _homeAssistant = new TEAMS2HA.API.HomeAssistant(_homeassistantURL, _homeassistantToken);
+
+            // Create a new instance of the HomeAssistant class with the Homeassistant URL and token
+            var homeAssistant = new TEAMS2HA.API.HomeAssistant(_settings.HomeAssistantURL, _homeassistantToken);
+
+            // Set the _homeAssistant variable to the same instance of the HomeAssistant class
+            _homeAssistant = new TEAMS2HA.API.HomeAssistant(_settings.HomeAssistantURL, _homeassistantToken);
+
+            // Check the connection to Homeassistant asynchronously and get the result
             var connectionSuccessful = await homeAssistant.CheckConnectionAsync();
+
+            // Update the HomeassistantConnectionStatus text based on the connection result
             HomeassistantConnectionStatus.Text = connectionSuccessful ? "Homeassistant: Connected" : "Homeassistant: Disconnected";
+
+            // If the connection is successful, start the HomeAssistant instance
             if (connectionSuccessful)
             {
                 await _homeAssistant.Start();
             }
         }
 
+        // This method is called when the TestTeamsConnection button is clicked
         private void TestTeamsConnection_Click(object sender, RoutedEventArgs e)
         {
-            string teamsToken = TokenStorage.GetTeamsToken();
-            // Implement the code to test the connection to Teams If the connection is successful,
-            // update the TeamsConnectionStatus
-            // Example: TeamsConnectionStatus.Text = "Teams: Connected";
+            string teamsToken = _settings.TeamsToken; // Get the Teams token from the settings
+
+            // Create a URI with the necessary parameters for the WebSocket connection
             var uri = new Uri($"ws://localhost:8124?token={teamsToken}&protocol-version=2.0.0&manufacturer=JimmyWhite&device=PC&app=THFHA&app-version=2.0.26");
 
             var state = new API.State();  // You would initialize this as necessary
-            _teamsClient = new API.WebSocketClient(uri, state);
+
+            // Create a new WebSocketClient with the URI, state, and settings file path
+            _teamsClient = new API.WebSocketClient(uri, state, _settingsFilePath);
+
+            // Subscribe to the ConnectionStatusChanged event of the WebSocketClient
             _teamsClient.ConnectionStatusChanged += TeamsConnectionStatusChanged;
-            //send output to log
-            _teamsClient.MessageReceived += (_teamsClient, message) =>
-            {
-                Debug.WriteLine(message);
-            };
         }
 
-      
         private void ApplyTheme(string theme)
         {
+            isDarkTheme = theme == "Dark";
             Uri themeUri;
             if (theme == "Dark")
             {
@@ -316,53 +301,32 @@ namespace TEAMS2HA
                 Application.Current.Resources.MergedDictionaries.Remove(currentTheme);
             }
         }
+
+        private async Task InitializeHomeAssistantConnection()
+        {
+            // Initialize HomeAssistant client with settings
+            _homeAssistant = new TEAMS2HA.API.HomeAssistant(_settings.HomeAssistantURL, _settings.HomeAssistantToken);
+
+            // Check the connection and update UI accordingly
+            var connectionSuccessful = await _homeAssistant.CheckConnectionAsync();
+            HomeassistantConnectionStatus.Text = connectionSuccessful ? "Homeassistant: Connected" : "Homeassistant: Disconnected";
+            if (connectionSuccessful)
+            {
+                await _homeAssistant.Start();
+            }
+        }
+
         private void ToggleThemeButton_Click(object sender, RoutedEventArgs e)
         {
             // Toggle the theme
             isDarkTheme = !isDarkTheme;
+            _settings.Theme = isDarkTheme ? "Dark" : "Light";
+            ApplyTheme(_settings.Theme);
 
-            // Update the theme setting
-            Properties.Settings.Default.Theme = isDarkTheme ? "Dark" : "Light";
-            
-
-            // Define the URIs for the dark and light themes
-            var darkThemeUri = new Uri("pack://application:,,,/MaterialDesignThemes.Wpf;component/Themes/MaterialDesignTheme.Dark.xaml");
-            var lightThemeUri = new Uri("pack://application:,,,/MaterialDesignThemes.Wpf;component/Themes/MaterialDesignTheme.Light.xaml");
-
-            // Determine the current theme URI based on the toggle
-            var themeUri = isDarkTheme ? darkThemeUri : lightThemeUri;
-
-            // Print the current merged dictionaries before the toggle
-            System.Diagnostics.Debug.WriteLine("Before toggle:");
-            foreach (var dictionary in Application.Current.Resources.MergedDictionaries)
-            {
-                System.Diagnostics.Debug.WriteLine($" - {dictionary.Source}");
-            }
-
-            // Check if the new theme already exists in the merged dictionaries
-            var existingTheme = Application.Current.Resources.MergedDictionaries.FirstOrDefault(d => d.Source == themeUri);
-            if (existingTheme == null)
-            {
-                // If the new theme does not exist, add it to the merged dictionaries
-                existingTheme = new ResourceDictionary() { Source = themeUri };
-                Application.Current.Resources.MergedDictionaries.Add(existingTheme);
-            }
-
-            // Remove the current theme from the merged dictionaries
-            var currentTheme = Application.Current.Resources.MergedDictionaries.FirstOrDefault(d => d.Source == (isDarkTheme ? lightThemeUri : darkThemeUri));
-            if (currentTheme != null)
-            {
-                Application.Current.Resources.MergedDictionaries.Remove(currentTheme);
-            }
-
-            // Print the current merged dictionaries after the toggle
-            System.Diagnostics.Debug.WriteLine("After toggle:");
-            foreach (var dictionary in Application.Current.Resources.MergedDictionaries)
-            {
-                System.Diagnostics.Debug.WriteLine($" - {dictionary.Source}");
-            }
+            // Save settings after changing the theme
             SaveSettings();
         }
+
         #endregion Private Methods
     }
 }
